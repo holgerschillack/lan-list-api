8e218cf6d3708a5bc3487a5f8cccf20d44ea77ef
Holger Schillack <hs@jochenlinnemann.de>
2023-03-03 10:47:55 +0100

    Update packages

--- index.js
+++ index.js
@@ -2,23 +2,21 @@ const find = require("local-devices");
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const CronJob = require("cron").CronJob;
+
+// Settings
 const PORT = 4100;
 const REFRESH_INTERVAL = "00 */5 * * * *";
-const SimpleNodeLogger = require("simple-node-logger"),
-  opts = {
-    timestampFormat: "YYYY-MM-DD HH:mm:ss",
-  },
-  log = SimpleNodeLogger.createSimpleLogger(opts);
-const CronJob = require("cron").CronJob;
 
 global.devices = [];
 
 function refreshDevices() {
-  log.info("Refreshing device list...");
+  console.log("Refreshing device list...");
   return new Promise((resolve, reject) => {
     find()
       .then((devList) => {
         devices = parseDevices(devList);
+        console.log(new Date(), "Done.");
         resolve(true);
       })
       .catch((err) => reject(err));
@@ -28,6 +26,7 @@ function refreshDevices() {
 function parseDevices(list) {
   const parsedDevices = [];
   list.forEach((dev) => {
+    console.log(dev);
     parsedDevices.push({
       ip: dev.ip,
       mac: dev.mac,
@@ -42,6 +41,11 @@ function parseDevices(list) {
 }
 
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Lan-List API, request /api/devices for device list.");
+});
+
 app.get("/api/devices", (req, res) => {
   res.json(devices);
 });
@@ -56,8 +60,8 @@ app.get("/api/refresh", (req, res) => {
     });
 });
 
-app.listen(PORT, () => {
-  console.log(`Lan list API started at Port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Lan List API started on port ${PORT}`);
 });
 
 const job = new CronJob(REFRESH_INTERVAL, function () {
